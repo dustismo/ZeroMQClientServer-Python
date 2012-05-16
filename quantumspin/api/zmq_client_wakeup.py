992734f89c2bc3280643dc7b1d93848b01d69548
Matt Dennebaum <matt@ninjacipher.com>
2012-05-16 07:57:14 -0400

    working on threading for wakeup class

--- quantumspin/api/zmq_client_wakeup.py
+++ quantumspin/api/zmq_client_wakeup.py
@@ -2,33 +2,30 @@
 	Created on May 15, 2012
 	@author: mattd
 '''
-import zmq, threading, uuid
+import zmq, threading, uuid, Queue
 
 class ZMQClientWakeup(threading.Thread):
 
 	def __init__(self,context,connection):
+		threading.Thread.__init__(self)
 		self.context = context
 		self.connection = connection
 		self.messages = Queue()
+		self.daemon = True
 
-	def start(self):
-		t = Thread(self)
-		t.setDaemon(True)
-		t.start()
-	
 	def run(self):
 		socket = self.context.socket(zmq.DEALER)
 		socket.setIdentity(uuid.uuid4())
 		socket.connect(self.connection);
 		while True:
 			try:
-				message = self.messages.take()
+				message = self.messages.get()
 				socket.send(message, 0)
 			except Exception,e:
 				print e.message
 
 	def send(self,message):
 		try:
-			self.messages.append(message)
+			self.messages.put(message)
 		except Exception,e:
 			print e.message
